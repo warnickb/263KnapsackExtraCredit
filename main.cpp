c8e49b38f0112ce04920d80a532ff1609e5251c4
warnickb <35404212+warnickb@users.noreply.github.com>
2018-04-06 12:16:07 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * @author Brendna Warnick
+ * @version CIS 263 Winter 2018
+ * 263 Extra Credit Knapsack Problem
+ * used https://www.geeksforgeeks.org/knapsack-problem/ for reference
+ */
+int max(int a, int b){
+	return (a > b)? a : b;
+}
+int knapSack(int W, int wt[], int val[], int n){
+	int i, w;
+	int K[n+1][W+1];
+	for (i = 0; i <= n; i++){
+		for (w = 0; w <= W; w++){
+           		if (i==0 || w==0)
+               			K[i][w] = 0;
+           		else if (wt[i-1] <= w)
+                 		K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+           		else
+                 		K[i][w] = K[i-1][w];
+       }
+   }
+ 
+   return K[n][W];
+}
+
+int main(){
+	int val[] = {100, 350, 225, 67, 275, 168};
+	int wt[] = {15, 25, 45, 30, 23, 37};
+	int W = 50;
+	int n = sizeof(val)/sizeof(val[0]);
+	printf("%d \n", knapSack(W, wt, val, n));
+	return 0;
+}
+
